587de9bd2fdb30d2216553eea3cad09f8608e2f0
Muna-Elmetwally <141143414+Muna-Elmetwally@users.noreply.github.com>
2024-11-21 23:32:54 +0200

    Delete files directory

--- files/js/slider.js
+++
@@ -1,8 +0,0 @@
-function scrollCarousel(direction) {
-  const carousel = document.querySelector('.slides');
-  const scrollAmount = carousel.scrollWidth / carousel.children.length * direction;
-  carousel.scrollBy({
-    left: scrollAmount,
-    behavior: 'smooth'
-  });
-}
